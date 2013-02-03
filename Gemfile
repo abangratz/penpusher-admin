1c4a4a2c8fbc34a6e1160a90eddf61b84d8106f8
ed wassermann <vis0vis@gmail.com>
2013-02-03 12:26:31 +0100

    Create first infrastructure

---
+++ Gemfile
@@ -0,0 +1,25 @@
+source 'https://rubygems.org'
+
+
+gem 'rails', '3.2.11'
+gem 'haml-rails'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'therubyracer', :platforms => :ruby
+  gem 'uglifier', '>= 1.0.3'
+end
+
+group :development, :test do
+  gem 'rspec-rails', git: 'https://github.com/rspec/rspec-rails'
+  gem 'capybara'
+  gem 'pry-rails'
+end
+
+group :test do
+  gem 'cucumber-rails', git: 'https://github.com/cucumber/cucumber-rails.git'
+end
+
+gem 'jquery-rails'
+gem 'unicorn'
+gem 'capistrano'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PenpusherAdmin::Application.config.secret_token = 'ed8f5b712fdedd5f9c98a1823e424d1c4059d1c3351f91d2b73a848b81b53cc9f7da6096a3ff51dd4f519457a96d852ba2fcb5aca511fc159c721739608c93f1'
