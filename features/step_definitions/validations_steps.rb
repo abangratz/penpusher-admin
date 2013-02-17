0562b18ab1b8097ed8dbbcff5ad7054b5f02bd66
Anton Bangratz <anton.bangratz@gmail.com>
2013-02-17 14:27:44 +0100

    Add checker for not having content

--- features/step_definitions/validations_steps.rb
+++ features/step_definitions/validations_steps.rb
@@ -2,6 +2,10 @@ Then /^I should see "(.*?)"$/ do |text|
   page.should have_content(text)
 end
 
+Then /^I should not see "(.*?)"$/ do |text|
+  page.should_not have_content(text)
+end
+
 Then /^I should be on the "(.*?)" page$/ do |path|
   current_path.should eq(path)
 end
