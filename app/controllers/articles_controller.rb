729a42fa7e070630f7a6b3fc105808e20011fc07
Anton Bangratz <anton.bangratz@gmail.com>
2013-02-17 14:15:37 +0100

    Add notice after creation

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,7 +15,7 @@ class ArticlesController < ApplicationController
 
   def create
     blog.create_article(params[:article][:title], params[:article][:body])
-    redirect_to articles_path
+    redirect_to articles_path, notice: "The article has been successfully saved."
   end
 
 end
