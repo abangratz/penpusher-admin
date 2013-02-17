b00846075164e00c32080a7c4d3d91b8a9fac453
Anton Bangratz <anton.bangratz@gmail.com>
2013-02-17 14:28:26 +0100

    Create articles from table

--- features/step_definitions/articles_steps.rb
+++ features/step_definitions/articles_steps.rb
@@ -1,6 +1,9 @@
 Given /^I have a list of articles with the following content:$/ do |table|
   table.hashes.each do |hash|
-    #TODO Create articles. How?
+    visit "/articles/new"
+    fill_in("Title", with: hash["title"])
+    fill_in("Body", with: hash["body"])
+    click_on("Save")
   end
 end
 
